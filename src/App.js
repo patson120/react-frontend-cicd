739413f86452325173f09c7fe6776fb1c74dbebe
patson120 <patrickkennenl@gmail.com>
2024-02-21 17:23:16 +0100

    My first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+const baseUrl = 'https://node-backend-cicd.onrender.com'
+
+function App() {
+
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    getNames()
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get(`${baseUrl}/names`)
+    setUserName(response.data.name)
+  }
+
+  return (
+    <>
+      <h1>My Website</h1>
+      <h3>My name is { userName } </h3>
+    </>
+  );
+}
+
+export default App;
